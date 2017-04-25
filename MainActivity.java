fdc7d416720af1a405c803f5b5b81a7248ec9889
isuri97 <isurianuradha96@gmail.com>
2017-04-25 18:50:57 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,21 +1,13 @@
-package com.example.isurianuradha.mynotification;
+package com.example.isurianuradha.splashscreen;
 
-import android.app.AlarmManager;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Button;
-import android.view.View.OnClickListener;
-
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,51 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-                Long alertTime = new GregorianCalendar().getTimeInMillis() + 5 * 1000;
-
-
-                AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-
-
-
-
-
-                /*Calendar calendar = Calendar.getInstance();
-
-
-                calendar.set(Calendar.HOUR_OF_DAY,10);
-                calendar.set(calendar.MINUTE,25);
-                 calendar.set(calendar.SECOND,30);*/
-
-
-                Intent intent = new Intent(getApplicationContext(), Notification_reciever.class);
-
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 100, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                alarmManager.set(AlarmManager.RTC_WAKEUP, alertTime,
-                        PendingIntent.getBroadcast(getApplicationContext(), 1, intent,
-                                PendingIntent.FLAG_UPDATE_CURRENT));
-
-
-
-                /*AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),AlarmManager.INTERVAL_DAY,pendingIntent);*/
-
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-
+        return super.onOptionsItemSelected(item);
+    }
+}
